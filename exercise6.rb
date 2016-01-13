4323fe5f851f91845f7d4a9845521c6e5cd51915
steveoneil <steve@steveoneil.com>
2016-01-13 16:49:26 -0500

    Incorporating list count and item checks

--- exercise6.rb
+++ exercise6.rb
@@ -10,6 +10,10 @@ def add_to_list(list, new_item)
   list << new_item
 end
 
+def check_list(list, check_item)
+  list.any? { |item| item == check_item }
+end
+
 grocery_list = ["tortilla shells", "ground beef", "limes", "salsa", "jalapeno peppers", "avocados"]
 print_list(grocery_list)
 puts " "
@@ -19,3 +23,10 @@ print_list(grocery_list)
 puts " "
 
 puts "There are #{grocery_list.count} items on the list"
+puts " "
+
+if check_list(grocery_list, "bananas")
+  puts "You don't need to pick up bananas today"
+else
+  puts "You need to pick up bananas"
+end
