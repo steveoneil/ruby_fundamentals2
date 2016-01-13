604c9635c75b7fe8d9d1a74050ab4167f9fe7208
steveoneil <steve@steveoneil.com>
2016-01-13 17:39:54 -0500

    Basic list print

---
+++ exercise7.rb
@@ -0,0 +1,16 @@
+#Bitmaker Cohort Exercise
+
+def cohort_display(cohort_list)
+  cohort_list.each do |cohort_num, student_count|
+    puts "#{cohort_num}: #{student_count} students"
+  end
+end
+
+
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+cohort_display(students)
