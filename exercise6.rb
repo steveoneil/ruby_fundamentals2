9c5417ab380481587deb009dcdbfb8a63f5a5011
steveoneil <steve@steveoneil.com>
2016-01-13 16:18:38 -0500

    Incorporating new methods (list & add)

--- exercise6.rb
+++ exercise6.rb
@@ -1,6 +1,21 @@
-#Grocery List
-grocery_list = ["tortilla shells", "ground beef", "limes", "salsa", "jalapeno peppers", "avocados"]
+#Grocery List Program
 
-grocery_list.each do |item|
-    puts "* #{item}"
+def print_list(list)
+  list.each do |item|
+  puts "* #{item}"
+  end
 end
+
+def add_to_list(list, new_item)
+  list << new_item
+end
+
+grocery_list = ["tortilla shells", "ground beef", "limes", "salsa", "jalapeno peppers", "avocados"]
+print_list(grocery_list)
+puts " "
+
+add_to_list(grocery_list, "rice")
+print_list(grocery_list)
+puts " "
+
+puts "There are #{grocery_list.count} items on the list"
