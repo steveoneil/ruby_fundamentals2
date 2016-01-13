6555d3b626970f74398bbbad058a2562157a576d
steveoneil <steve@steveoneil.com>
2016-01-13 14:36:36 -0500

    F to C conversion functioning

--- exercise5.rb
+++ exercise5.rb
@@ -1,1 +1,9 @@
-test
+#Method to convert Fahrenheit to Celsius
+def fahr_to_Cel(f)
+    c = (f.to_i - 32) * 5 / 9
+    puts "#{f} degrees Fahrenheit is equal to #{c} degrees Celsius"
+end
+
+puts "Please enter a number representing a temperature in Fahrenheit"
+f_temp = gets.chomp
+fahr_to_Cel(f_temp)
